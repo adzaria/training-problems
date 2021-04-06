73489af510d058a3fec33e818a9121bcf298a4a8
adzaria <adzaria@protonmail.com>
2021-04-07 01:54:00 +0300

    added a solution to two-sum-ii in python

---
+++ Leetcode/two-sum-ii-input-array-is-sorted/twoSum--good.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/ Given an array of integers numbers that
+# is already sorted in ascending order, find two numbers such that they add up to a specific target number. Return
+# the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1]
+# <= numbers.length. You may assume that each input would have exactly one solution and you may not use the same
+# element twice.
+#
+# Runtime: 72ms (faster than 99%)
+# Memory: 40mo (less than 20%)
+
+class Solution:
+    def twoSum(self, numbers, target):
+        i = 0
+        j = len(numbers) - 1
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i + 1, j + 1]
+            if numbers[i] + numbers[j] < target:
+                i += 1
+            if numbers[i] + numbers[j] > target:
+                j -= 1
